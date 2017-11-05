0161859461cec24c136c473de529218b711c2b71
Ludwig Frank <lufra2606@gmail.com>
2017-11-05 19:27:10 +0100

    add components and container index files

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-export default () => <div> Welcomeff to next </div>
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 20px;
+`
+
+export default () => <Title>My page</Title>
